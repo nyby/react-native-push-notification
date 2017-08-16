2153596f8eff8f359da69ded06ff5ed514406907
Mateusz Murza <mateusz.murza@netbulls.io>
2017-08-16 12:37:02 +0200

    Remove unnecessary Notification Id from RNPushNotificationPublisher

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationPublisher.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationPublisher.java
@@ -13,30 +13,29 @@ import java.util.List;
 import static com.dieam.reactnativepushnotification.modules.RNPushNotification.LOG_TAG;
 
 public class RNPushNotificationPublisher extends BroadcastReceiver {
-    final static String NOTIFICATION_ID = "notificationId";
+	final static String NOTIFICATION_ID = "notificationId";
+	final static int NOTIFICATION_NOT_FOUND = -1;
 
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        int id = intent.getIntExtra(NOTIFICATION_ID, 0);
-        long currentTime = System.currentTimeMillis();
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		int notificationId = intent.getIntExtra(NOTIFICATION_ID, NOTIFICATION_NOT_FOUND);
+		if(notificationId == -1)
+			return;
+		long currentTime = System.currentTimeMillis();
+		Log.i(LOG_TAG, "NotificationPublisher: Prepare To Publish: " + notificationId + ", Now Time: " + currentTime);
 
-        Log.i(LOG_TAG, "NotificationPublisher: Prepare To Publish: " + id + ", Now Time: " + currentTime);
-
-        Application applicationContext = (Application) context.getApplicationContext();
-        Class intentClass = getMainActivityClass(context);
-        if (this.isApplicationInForeground(context)) {
-            Intent newIntent = new Intent(context, intentClass);
-            newIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-            newIntent.putExtra("notification", intent.getExtras());
-            newIntent.putExtra("foreground", true);
-            String notificationIdString = intent.getExtras().getString("id");
-            int notificationID = Integer.parseInt(notificationIdString);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, notificationID, newIntent,
-                    PendingIntent.FLAG_UPDATE_CURRENT);
-            try {
-                pendingIntent.send(context, 0, newIntent);
-				new RNPushNotificationHelper(applicationContext).removeFromSharedPrefs(notificationIdString);
-            } catch (PendingIntent.CanceledException e) {
+		Application applicationContext = (Application) context.getApplicationContext();
+		Class intentClass = getMainActivityClass(context);
+		if (this.isApplicationInForeground(context)) {
+			Intent newIntent = new Intent(context, intentClass);
+			newIntent.putExtra("notification", intent.getExtras());
+			newIntent.putExtra("foreground", true);
+			PendingIntent pendingIntent = PendingIntent.getActivity(context, notificationId, newIntent,
+					PendingIntent.FLAG_UPDATE_CURRENT);
+			try {
+				pendingIntent.send(context, 0, newIntent);
+				new RNPushNotificationHelper(applicationContext).removeFromSharedPrefs(String.valueOf(notificationId));
+			} catch (PendingIntent.CanceledException e) {
 				Log.i(LOG_TAG, "NotificationPublisher: Intent Canceled");
             }
         } else
